33aa81ceada3deda7da0f8fc5863d39685e31b5f
Diego Cardoso <diegocar448@hotmail.com>
2022-04-28 09:41:30 -0300

    adicionando permissao no AndroidManifest(main/debug/profile) | pacotes (google_maps_flutter/geolocator/geocoding/permission_handler) | setando /android/app/src/build.gradle -> defaultConfig{ minSdkVersion 20 }

--- lib/app/data/base_url.dart
+++ lib/app/data/base_url.dart
@@ -1,2 +1,2 @@
 //const baseUrl = 'http://barbearialaravel_api.test/api';
-const baseUrl = 'https://c332-179-126-65-247.sa.ngrok.io/api';
+const baseUrl = 'https://065b-177-106-230-70.sa.ngrok.io/api';
